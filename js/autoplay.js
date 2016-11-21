300f2c61ce1776f4d9beab2ccb9ee1f13077df6a
Sergey Shevchenko <sshevchenko@cogniance.com>
2016-11-21 20:22:24 +0200

    fix

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,4 +1,46 @@
 $(function () {
+	var locals = {
+		year: (new Date()).getFullYear(),
+		download_setup: 'download/autoplay_setup.exe',
+		order_personal: 'https://secure.payproglobal.com/orderpage.aspx?products=47884',
+		order_site: 'https://secure.payproglobal.com/orderpage.aspx?products=47887'
+	};
+	var routes = {
+		index: { id: 'content/index', locals: locals },
+		overview: { id: 'content/overview', locals: locals },
+		download: { id: 'content/download', locals: locals },
+		contact: { id: 'content/contact', locals: locals },
+		order: { id: 'content/order', locals: locals }
+	};
+	var contentElement = document.getElementById('page-content');
+
+	function onHashChange() {
+		var hash = location.hash.slice(1) || 'index';
+		var route = routes[hash] || routes['index'];
+		if (route) {
+			contentElement.innerHTML = parseTemplate(route.id, route.locals);
+			$('#header li').removeClass('active');
+			$('#header a[href="#' + hash + '"]').parent().addClass('active');
+		}
+	}
+
+	function parseTemplate(id, locals) {
+		var html = document.getElementById(id).innerHTML;
+		Object.keys(locals).forEach(function (key) {
+			var regExp = new RegExp('{{' + key + '}}', 'g');
+			html = html.replace(regExp, locals[key]);
+		});
+		return html;
+	}
+
+	(function init() {
+		document.getElementById('footer').innerHTML = parseTemplate('footer', locals);
+		onHashChange();
+		window.addEventListener('hashchange', onHashChange);
+	})();
+});
+
+$(function () {
 	$('.lic-info').popover();
 
 	$('#contact_us').on('submit', function(e) {
